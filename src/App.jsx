796a006241caafa5ecb6875ff17c3cc623503704
Nata Kalina <natakalinatester@gmail.com>
2024-02-29 23:31:42 -0500

    Lists in React

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,35 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const todoList = [
+  {
+    id: '1',
+    title: 'Complete lesson 1.1 React',
+  },
+  {
+    id: '2',
+    title: 'Complete lesson 1.1 Python',
+  },
+  {
+    id: '3',
+    title: 'Complete lesson 1.1 DSA',
+  },
+
+];
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header style={{ textAlign: 'center' }}>
+        <h1>Todo List</h1>
+      </header>
+      <ul style={{ textAlign: 'left' }}>
+        {todoList.map(function (item) {
+          return <li key={item.id}>{item.title}</li>;
+        })}
+      </ul>
     </>
   )
 }
