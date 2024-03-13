866030ed57a6cb6b0577df42f82f97b357bae910
Nata Kalina <natakalinatester@gmail.com>
2024-03-13 18:53:41 -0400

    Props, State, And Handlers

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const AddTodoForm = () => {
+const AddTodoForm = (props) => {
+
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    const todoTitle = event.target.title.value;
+    props.onAddTodo(todoTitle);
+    console.log(todoTitle);
+    event.target.reset();
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleAddTodo}>
         <label htmlFor="todoTitle">Title: </label>
         <input id="todoTitle" type="text" name="title" />
         <button type="submit">Add</button>
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react';
+import React from 'react';
 import './App.css';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [newTodo, setNewTodo] = React.useState('');
 
   return (
     <>
       <header style={{ textAlign: 'center' }}>
         <h1>Todo List</h1>
       </header>
-      <AddTodoForm />
+      <AddTodoForm onAddTodo={setNewTodo} />
+      <p style={{ textAlign: 'left' }}>
+        <span> New Task: </span>
+        {newTodo}
+      </p>
       <TodoList />
     </>
   );
